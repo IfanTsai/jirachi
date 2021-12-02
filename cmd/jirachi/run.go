deeb9b60f624603873e4f2acfaa39008653910e7
IfanTsai <i@caiyifan.cn>
2021-12-02 20:48:29 +0800

    add GNU style REPL

--- cmd/jirachi/run.go
+++ cmd/jirachi/run.go
@@ -1,24 +1,14 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
 	"os"
 
-	"github.com/IfanTsai/jirachi/interpreter"
+	"github.com/IfanTsai/jirachi/repl"
 )
 
 func main() {
-	reader := bufio.NewScanner(os.Stdin)
-
-	for {
-		fmt.Printf("jirachi > ")
-		reader.Scan()
-		ast, err := interpreter.Run("stdin", reader.Text())
-		if err != nil {
-			fmt.Printf("%+v\n", err)
-		} else {
-			fmt.Printf("%v\n", ast)
-		}
+	if len(os.Args) == 1 {
+		repl.Run()
 	}
+	// TODO: execute source file
 }
---
+++ repl/repl.go
@@ -0,0 +1,40 @@
+package repl
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/IfanTsai/jirachi/interpreter"
+	"github.com/chzyer/readline"
+)
+
+func Run() {
+	reader, err := readline.NewEx(&readline.Config{
+		Prompt:            "\u001B[33mjirachi\u001B[0m \033[32m»\033[0m ",
+		HistoryFile:       "/tmp/.jirachi_repl.tmp",
+		HistorySearchFold: true,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	defer reader.Close()
+
+	for {
+		line, err := reader.Readline()
+		if err != nil {
+			if !errors.Is(err, readline.ErrInterrupt) {
+				fmt.Println(err)
+			}
+
+			break
+		}
+
+		ast, err := interpreter.Run("stdin", line)
+		if err != nil {
+			fmt.Printf("%+v\n", err)
+		} else {
+			fmt.Printf("%v\n", ast)
+		}
+	}
+}
