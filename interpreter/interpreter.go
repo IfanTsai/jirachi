714033587c4f405729a2423674bde7e9f7cef8ba
IfanTsai <i@caiyifan.cn>
2021-12-06 01:00:01 +0800

    refacto JNode type to interface

--- interpreter/interpreter.go
+++ interpreter/interpreter.go
@@ -48,40 +48,40 @@ func NewJInterpreter(context *common.JContext) *JInterpreter {
 	}
 }
 
-func (i *JInterpreter) Interpreter(ast *parser.JNode) (*JNumber, error) {
+func (i *JInterpreter) Interpreter(ast parser.JNode) (*JNumber, error) {
 	return i.visit(ast)
 }
 
-func (i *JInterpreter) visit(node *parser.JNode) (*JNumber, error) {
-	switch node.Type {
+func (i *JInterpreter) visit(node parser.JNode) (*JNumber, error) {
+	switch node.Type() {
 	case parser.Number:
-		return i.visitNumberNode(node)
+		return i.visitNumberNode(node.(*parser.JNumberNode))
 	case parser.BinOp:
-		return i.visitBinOpNode(node)
+		return i.visitBinOpNode(node.(*parser.JBinOpNode))
 	case parser.UnaryOp:
-		return i.visitUnaryOpNode(node)
+		return i.visitUnaryOpNode(node.(*parser.JUnaryOpNode))
 	case parser.VarAssign:
-		return i.visitVarAssignNode(node)
+		return i.visitVarAssignNode(node.(*parser.JVarAssignNode))
 	case parser.VarAccess:
-		return i.visitVarAccessNode(node)
+		return i.visitVarAccessNode(node.(*parser.JVarAccessNode))
 	case parser.IfExpr:
-		return i.visitIfExprNode(node)
+		return i.visitIfExprNode(node.(*parser.JIfExprNode))
 	default:
 		return nil, errors.Wrap(&common.JInvalidSyntaxError{
 			JError: &common.JError{
-				StartPos: node.StartPos,
-				EndPos:   node.EndPos,
+				StartPos: node.GetStartPos(),
+				EndPos:   node.GetEndPos(),
 			},
 			Details: "Expected number, bin op or unary op",
 		}, "failed to visit node")
 	}
 }
 
-func (i *JInterpreter) visitNumberNode(node *parser.JNode) (*JNumber, error) {
+func (i *JInterpreter) visitNumberNode(node *parser.JNumberNode) (*JNumber, error) {
 	return NewJNumber(node.Token.Value).SetJPos(node.StartPos, node.EndPos).SetJContext(i.Context), nil
 }
 
-func (i *JInterpreter) visitVarAssignNode(node *parser.JNode) (*JNumber, error) {
+func (i *JInterpreter) visitVarAssignNode(node *parser.JVarAssignNode) (*JNumber, error) {
 	varName := node.Token.Value
 
 	value, err := i.visit(node.Node)
@@ -94,7 +94,7 @@ func (i *JInterpreter) visitVarAssignNode(node *parser.JNode) (*JNumber, error) 
 	return value, nil
 }
 
-func (i *JInterpreter) visitIfExprNode(node *parser.JNode) (*JNumber, error) {
+func (i *JInterpreter) visitIfExprNode(node *parser.JIfExprNode) (*JNumber, error) {
 	for index := range node.Cases {
 		condition := node.Cases[index][0]
 		expr := node.Cases[index][1]
@@ -127,7 +127,7 @@ func (i *JInterpreter) visitIfExprNode(node *parser.JNode) (*JNumber, error) {
 	return NewJNumber(nil), nil
 }
 
-func (i *JInterpreter) visitVarAccessNode(node *parser.JNode) (*JNumber, error) {
+func (i *JInterpreter) visitVarAccessNode(node *parser.JVarAccessNode) (*JNumber, error) {
 	varName := node.Token.Value
 	value := i.Context.SymbolTable.Get(varName)
 	if value == nil {
@@ -144,7 +144,7 @@ func (i *JInterpreter) visitVarAccessNode(node *parser.JNode) (*JNumber, error) 
 	return NewJNumber(value.(*JNumber).Value).SetJPos(node.StartPos, node.EndPos).SetJContext(i.Context), nil
 }
 
-func (i *JInterpreter) visitBinOpNode(node *parser.JNode) (*JNumber, error) {
+func (i *JInterpreter) visitBinOpNode(node *parser.JBinOpNode) (*JNumber, error) {
 	leftNumber, err := i.visit(node.LeftNode)
 	if err != nil {
 		return nil, err
@@ -204,7 +204,7 @@ func (i *JInterpreter) visitBinOpNode(node *parser.JNode) (*JNumber, error) {
 	return resNumber.SetJPos(node.StartPos, node.EndPos), nil
 }
 
-func (i *JInterpreter) visitUnaryOpNode(node *parser.JNode) (*JNumber, error) {
+func (i *JInterpreter) visitUnaryOpNode(node *parser.JUnaryOpNode) (*JNumber, error) {
 	number, err := i.visit(node.Node)
 	if err != nil {
 		return nil, err
